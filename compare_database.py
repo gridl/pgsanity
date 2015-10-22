5f0b3327e9d6d69e2d5218f0f136b3a73740f7ef
Iryna Cherniavska <i.chernyavska@gmail.com>
2015-10-22 10:58:16 -0700

    Added more output options.

--- compare_database.py
+++ compare_database.py
@@ -13,15 +13,19 @@ def read_data(filename):
         return json.load(f)
 
 
-def yay(text):
-    print(colored(text, 'green'))
+def yay(text, color):
+    if color:
+        text = colored(text, 'green')
+    print(text)
 
 
-def nay(text):
-    print(colored(text, 'red'))
+def nay(text, color):
+    if color:
+        text = colored(text, 'red')
+    print(text)
 
 
-def compare_values(msg, expected, actual, approved_diff=None):
+def compare_values(msg, expected, actual, approved_diff=None, diff_only=True, color=False):
 
     def close_enough(exp, act):
         if not approved_diff:
@@ -32,12 +36,13 @@ def compare_values(msg, expected, actual, approved_diff=None):
         return actual_diff < expected_diff
 
     if expected == actual or close_enough(expected, actual):
-        yay('{0}: same: expected {1}, actual {2}.'.format(msg, expected, actual))
+        if not diff_only:
+            yay('{0}: same: expected {1}, actual {2}.'.format(msg, expected, actual), color)
     else:
-        nay('{0}: different: expected {1}, actual {2}.'.format(msg, expected, actual))
+        nay('{0}: different: expected {1}, actual {2}.'.format(msg, expected, actual), color)
 
 
-def compare_data(ref_data, table_data):
+def compare_data(ref_data, table_data, diff_only, color):
     compare_values('Number of tables', len(ref_data), len(table_data))
 
     for table_name in sorted(ref_data.keys()):
@@ -58,16 +63,20 @@ def compare_data(ref_data, table_data):
                         compare_values(
                             '  {0} min'.format(col_name),
                             ref_data[table_name]['columns'][col_name]['min_value'],
-                            table_data[table_name]['columns'][col_name]['min_value'])
+                            table_data[table_name]['columns'][col_name]['min_value'],
+                            diff_only=diff_only,
+                            color=color)
 
                         compare_values(
                             '  {0} max'.format(col_name),
                             ref_data[table_name]['columns'][col_name]['max_value'],
-                            table_data[table_name]['columns'][col_name]['max_value'])
+                            table_data[table_name]['columns'][col_name]['max_value'],
+                            diff_only=diff_only,
+                            color=color)
                 else:
-                    nay('  {0} field is missing.'.format(col_name))
+                    nay('  {0} field is missing.'.format(col_name), color)
         else:
-            nay('* {0} table is missing.'.format(table_name))
+            nay('* {0} table is missing.'.format(table_name), color)
 
 
 if __name__ == '__main__':
@@ -82,6 +91,17 @@ if __name__ == '__main__':
         '--filename',
         dest='filename',
         help='Input JSON filename (required).')
+    parser.add_option(
+        '-c',
+        '--colored',
+        dest='colored',
+        default=False,
+        help='Color output.')
+    parser.add_option(
+        '--diff-only',
+        dest='diff_only',
+        default=True,
+        help='Only print out what is different.')
     (popts, pargs) = parser.parse_args()
 
     if not popts.database or not popts.filename:
@@ -91,4 +111,4 @@ if __name__ == '__main__':
     base, session = create_database_session(popts.database)
     reference_data = read_data(popts.filename)
     table_data = collect_table_data(base, session)
-    compare_data(reference_data, table_data)
+    compare_data(reference_data, table_data, popts.diff_only, popts.colored)
